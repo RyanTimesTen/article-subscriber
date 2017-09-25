a3889307ef2a69e24875db1ae4cce4cc66bf4adc
Ryan <ryangilbert7926@gmail.com>
2017-09-24 23:10:08 -0400

    Adds target blog as a source

--- src/main/kotlin/api/v1/Sources.kt
+++ src/main/kotlin/api/v1/Sources.kt
@@ -6,5 +6,6 @@ val TIME_TECH = "time-tech"
 val TWILIO_BLOG = "twilio-blog"
 val TWILIO_PRESS = "twilio-press"
 val THUMBTACK_BLOG = "thumbtack-blog"
+val TARGET_BLGO = "target-blog"
 
-val SOURCES = listOf(FACEBOOK_BLOG, TIME_TECH, TWILIO_BLOG, TWILIO_PRESS, THUMBTACK_BLOG)
+val SOURCES = listOf(FACEBOOK_BLOG, TIME_TECH, TWILIO_BLOG, TWILIO_PRESS, THUMBTACK_BLOG, TARGET_BLGO)
