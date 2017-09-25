9cd24d719f31142ec4a3ba0bb22d3ee67068b298
Ryan <ryangilbert7926@gmail.com>
2017-09-24 23:29:21 -0400

    Fixes typo

--- src/main/kotlin/api/v1/Sources.kt
+++ src/main/kotlin/api/v1/Sources.kt
@@ -6,6 +6,6 @@ val TIME_TECH = "time-tech"
 val TWILIO_BLOG = "twilio-blog"
 val TWILIO_PRESS = "twilio-press"
 val THUMBTACK_BLOG = "thumbtack-blog"
-val TARGET_BLGO = "target-blog"
+val TARGET_BLOG = "target-blog"
 
-val SOURCES = listOf(FACEBOOK_BLOG, TIME_TECH, TWILIO_BLOG, TWILIO_PRESS, THUMBTACK_BLOG, TARGET_BLGO)
+val SOURCES = listOf(FACEBOOK_BLOG, TIME_TECH, TWILIO_BLOG, TWILIO_PRESS, THUMBTACK_BLOG, TARGET_BLOG)
