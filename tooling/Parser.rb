0cd09709071031ca219db6f709dc509b70e9d0b6
Ryan <ryangilbert7926@gmail.com>
2017-09-24 23:05:04 -0400

    Adds xpath for target blog

--- tooling/Parser.rb
+++ tooling/Parser.rb
@@ -4,7 +4,11 @@ class Parser
     $logger.info("Attempting to parse html for source: #{source[:name]}")
     begin
       Nokogiri::HTML(open(source[:base_url])).xpath(source[:xpath]).map do |link|
-        { :name => link.text, :link => link['href'] }
+        if source[:base_url].include? 'target'
+          { :name => link.text, :link => link['href'][2..-1] }
+        else
+          { :name => link.text, :link => link['href'] }
+        end
       end
     rescue => e
       $logger.error("Error when parsing html for source: #{source[:name]}: #{e}")
